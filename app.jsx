86c329f678e3614d3bcb08954ea81b44f56af68d
seonghyeon <hyeon@apartmentary.com>
2018-01-16 10:40:50 +0900

    setting css-loader options to make className shortest

--- app.jsx
+++ app.jsx
@@ -1,11 +1,16 @@
-import './style';
+import style from './style';
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 
 const App = () => (
-  <div>Hello World</div>
+  <div>
+    <div className={style.blue}>Hello World</div>
+    <div className={style.red}>Hello World</div>
+    <div className={style.green}>Hello World</div>
+    <div className="black">Hello World</div>
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,37 @@
 const path = require('path');
+const incstr = require('incstr');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const createUniqueIdGenerator = () => {
+  const index = {};
+  const generateNextId = incstr.idGenerator({
+    alphabet: 'abcefghijklmnopqrstuvwxyz0123456789'
+  });
+
+  return (name) => {
+    if (index[name]) {
+      return index[name];
+    }
+    let nextId;
+
+    // className should be started with Numbers
+    do {
+      nextId = generateNextId();
+    } while (/^[0-9]/.test(nextId));
+
+    index[name] = generateNextId();
+
+    return index[name];
+  };
+};
+
+const uniqueIdGenerator = createUniqueIdGenerator();
+const generateScopedName = (localName, resourcePath) => {
+  const componentName = resourcePath.split('/').slice(-2, -1);
+  return `${uniqueIdGenerator(componentName)}_${uniqueIdGenerator(localName)}`
+};
+
 
 module.exports = {
   resolve: {
@@ -20,7 +51,22 @@ module.exports = {
       },
       {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+              importLoaders: 1,
+              minimize: true,
+              getLocalIdent: (context, localIdentName, localName) => {
+                return generateScopedName(localName, context.resourcePath);
+              },
+            }
+          },
+          'less-loader',
+        ],
       },
     ],
   },
@@ -30,5 +76,4 @@ module.exports = {
       template: './index.html',
     }),
   ],
-
 };
